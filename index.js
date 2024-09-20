143bc6d353b779f6ae5842870e2b876ad4c309fb
Coffee_sw <897884964@qq.com>
2024-09-20 12:55:12 +0800

    🎉 init:init

---
+++ index.js
@@ -0,0 +1,76 @@
+const fs = require("fs");
+const path = require("path");
+const sharp = require("sharp");
+
+const sourceUrl = `https://assets.tarkov.dev/maps/customs_v4/main`;
+const imgsSavePath = path.join(__dirname, "imgs");
+
+let counter = 0;
+
+const ensureLevelPathExist = async (level) => {
+  const dirPath = path.join(imgsSavePath, `${level}`);
+  try {
+    const stats = fs.statSync(dirPath);
+    if (stats.isDirectory()) return;
+  } catch (err) {
+    if (err.code === "ENOENT") {
+      fs.mkdirSync(dirPath);
+    }
+  }
+};
+
+const fetchImgs = async (level, x, y) => {
+  const url = `${sourceUrl}/${level}/${x}/${y}.png`;
+  ensureLevelPathExist(level);
+  const localPath = path.join(imgsSavePath, `${level}`, `${x}-${y}.png`);
+  const response = await fetch(url);
+  if (response.status == "404") {
+    const bufferPng = await sharp({
+      create: {
+        width: 256,
+        height: 256,
+        channels: 4,
+        background: { r: 255, g: 255, b: 255, alpha: 0.5 },
+      },
+    })
+      .png()
+      .toBuffer();
+    fs.writeFileSync(localPath, bufferPng);
+  } else {
+    const arrayBuffer = await response.arrayBuffer();
+    const buffer = Buffer.from(arrayBuffer);
+    fs.writeFileSync(localPath, buffer);
+  }
+  console.log(`${level},${x}-${y}.png saved success.`);
+  counter++;
+};
+
+const levelPicSizeMap = new Map();
+levelPicSizeMap.set(0, [1, 1]);
+levelPicSizeMap.set(1, [1, 1]);
+levelPicSizeMap.set(2, [1, 1]);
+levelPicSizeMap.set(3, [1, 1]);
+levelPicSizeMap.set(4, [16, 13]);
+levelPicSizeMap.set(5, [1, 1]);
+levelPicSizeMap.set(6, [64, 49]);
+
+const fetchCols = async (level, x) => {
+  const ySize = levelPicSizeMap.get(level)[1];
+
+  for (let y = 15, len = ySize; y < len; y++) {
+    await fetchImgs(level, x, y);
+  }
+};
+
+const fetchLevel = async (level) => {
+  const xSize = levelPicSizeMap.get(level)[0];
+  for (let x = 0, len = xSize; x < len; x++) {
+    await fetchCols(level, x);
+  }
+};
+const main = async () => {
+  await fetchLevel(6);
+  console.log(counter);
+};
+
+main();
---
+++ move.js
@@ -0,0 +1,43 @@
+const { resolve6 } = require("dns");
+const fs = require("fs");
+const path = require("path");
+
+const outPath = "6-x";
+const soucePath = "imgs/6";
+
+const ensurePathExist = async (dirPath) => {
+  try {
+    const stats = fs.statSync(dirPath);
+    if (stats.isDirectory()) return;
+  } catch (err) {
+    if (err.code === "ENOENT") {
+      fs.mkdirSync(dirPath);
+    }
+  }
+};
+
+const moveFile = () => {
+  const fileList = fs.readdirSync(path.join(__dirname, soucePath));
+  console.log(fileList);
+  fileList.forEach((fileName) => {
+    const match = fileName.match(/\d+/g);
+    ensurePathExist(path.join(__dirname, outPath, match[0]));
+    const sourceFilePath = path.join(__dirname, soucePath, fileName);
+    const destinationPath = path.join(__dirname, outPath, match[0], fileName);
+    fs.renameSync(sourceFilePath, destinationPath);
+  });
+};
+
+const fileCount = () => {
+  const res = fs.readdirSync(outPath);
+  console.log(outPath, `,has`, res.length, `dir`);
+  res.forEach((dir) => {
+    const p = path.join(outPath, dir);
+    const fileList = fs.readdirSync(p);
+    console.log(p, `has`, fileList.length, `file`);
+  });
+};
+const main = () => {
+  fileCount();
+};
+main();
